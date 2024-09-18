b1a0e6e6261cf0dfa95c0d1d7fcb6b9f3a664f9c
Abi Mengesha <abbymengesha@Abbys-MacBook-Pro-2.local>
2024-09-18 15:41:07 -0700

    Never used go be4 so starting with HELLOWORLD

---
+++ hello.go
@@ -0,0 +1,11 @@
+/*Never used GO-lang before so starting with our fav
+hello world*/
+
+package main /* a package is a way to group functions
+main has to be executable and is a stand-alone application*/
+
+import "fmt" // a package in go used for formatting I/O
+
+func main() {
+	fmt.Println("Hello, World!")
+}
