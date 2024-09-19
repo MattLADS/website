9871ed9a76621e560a722af343fa2d55baebe82c
Abi Mengesha <abbymengesha@Abbys-MacBook-Pro-2.local>
2024-09-18 18:39:14 -0700

    Pushing everything.

--- hello.go
+++ hello.go
@@ -1,11 +1,11 @@
-/*Never used GO-lang before so starting with our fav
-hello world*/
+// /*Never used GO-lang before so starting with our fav
+// hello world*/
 
-package main /* a package is a way to group functions
-main has to be executable and is a stand-alone application*/
+// package main /* a package is a way to group functions
+// main has to be executable and is a stand-alone application*/
 
-import "fmt" // a package in go used for formatting I/O
+// import "fmt" // a package in go used for formatting I/O
 
-func main() {
-	fmt.Println("Hello, World!")
-}
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
